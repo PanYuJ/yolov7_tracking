397a85abfda8a33b967ee98b521828662d870890
PanYuJ <109787213+PanYuJ@users.noreply.github.com>
2023-02-12 21:02:30 +0800

    Update tracker_custom.py

--- tracker/tracker_custom.py
+++ tracker/tracker_custom.py
@@ -22,20 +22,13 @@ from uavmot import UAVMOT
 from strongsort import StrongSORT
 
 try:  # import package that outside the tracker folder  For yolo v7
-    import sys 
     sys.path.append(os.getcwd())   
-    from models.experimental import attempt_load
     # from utils.datasets import LoadWebcam
-    from utils.general import check_imshow
-    from evaluate import evaluate
-    from utils.torch_utils import select_device, time_synchronized, TracedModel
     print('Note: running yolo v7 detector')
 
 except:
     pass
-
 import tracker_dataloader
-import trackeval
 
 def set_basic_params(cfgs):
     global CATEGORY_DICT, DATASET_ROOT, CERTAIN_SEQS, IGNORE_SEQS, YAML_DICT
@@ -157,7 +150,7 @@ def tracker_custom(opts, cfgs):
                       cur_tlwh.append(bbox)
                       cur_id.append(id)
                       cur_cls.append(cls)
-                      # results.append((frame_id + 1, id, bbox, cls))
+                      
 
               results.append((frame_id + 1, cur_id, cur_tlwh, cur_cls))
               timer.toc()  # end timing this image
@@ -190,10 +183,9 @@ def tracker_custom(opts, cfgs):
         cap =cv2.VideoCapture(0)
         start_time = time.time()
         fps_counter = 0
-        fps = cap.get(cv2.CAP_PROP_FPS)
+        
         
         while cap.isOpened() :
-            # time.sleep(1.0)
             success, im0s = cap.read()
             
             if not success:
@@ -238,7 +230,6 @@ def tracker_custom(opts, cfgs):
             start_time = time.time()
             frame_id += 1
             fps_counter = 0
-            
             
         cap.release()
         cv2.destroyAllWindows()
@@ -297,23 +288,13 @@ def tracker_custom(opts, cfgs):
                 for trk in current_tracks:
                     box = trk.tlwh # x_bottom_left, y_bottom_left, width, height
                     id = trk.track_id
-                    
                     cls = trk.cls
                     
-                # filter low area bbox
-                    # if bbox[2] * bbox[3] > opts.min_area:
+                    # filter low area bbox
                     if box[2] * box[3] > opts.min_area:
-                      # cur_tlwh.append(bbox)
-                      # cur_id.append(id)
-                      # cur_cls.append(cls)
-                      # memory[cur_id[-1]] = cur_tlwh[-1]
                       memory[id] = box
                       
-                # results = [cur_tlwh, cur_id, cur_cls]
-                # tlwhs, ids, clss = results[0], results[1], results[2]
-
                     # Current frame
-                # for i, (box, id, cls) in enumerate(zip(tlwhs, ids, clss)):
                     center = tuple([int(box[0]+(box[2]/2)), int(box[1]+(box[3]/2))])
                     
                     # draw a rect
@@ -321,23 +302,24 @@ def tracker_custom(opts, cfgs):
                     cv2.circle(img_, center, 4, get_color(id), thickness=-1)
                     color_current = sub_img[center[1], center[0]]
                     
-                    print('current_id:',id, color_current)
+                    # print('current_id:',id, color_current)
                     
                     # note the id and cls
                     text = f'{CATEGORY_DICT[cls]}-{id}'
                     cv2.putText(img_, text, [int(box[0]), int(box[1])], fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=1, 
                                     color=get_color(id), thickness=1)
-                
+                    cv2.setMouseCallback('FRAME', POINTS)
                     # previous frame
                     if id in previous:
                         previous_box = previous[id]
                         
                         # convert tlwh to tlbr
-                        # previous_tlbr = tuple([int(previous_box[0]), int(previous_box[1]), int(previous_box[0] + previous_box[2]), int(previous_box[1] + previous_box[3])])
+                        
                         previous_center = tuple([int(previous_box[0]+(previous_box[2]/2)), int(previous_box[1]+int(previous_box[3]/2))])
                         
                         color_previous = sub_img[previous_center[1], previous_center[0]]
-                        print('previous_id:',id, color_previous)
+                        # print('previous_id:',id, color_previous)
+                        
                         # UP_south = (color_current == [0,0,255]).all() and (color_previous == [0,255,0]).all()
                 
                         DOWN_south = (color_previous == [0,0,255]).all() and (color_current==[0,255,0]).all()
@@ -362,38 +344,30 @@ def tracker_custom(opts, cfgs):
                             if CATEGORY_DICT[cls]=='truck':
                                 truck_north += 1
                     
-                    cv2.putText(img_, f'car: {car_south}\nbus: {bus_south}\ntruck: {truck_south}', (200, 500), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2, 
+                    cv2.putText(img_, f'car: {car_south}', (200, 50), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2, 
                                     color=(0, 0, 255), thickness=4)
                     
-                    cv2.putText(img_, f'car: {car_north}\nbus: {bus_north}\ntruck: {truck_north}', (500, 500), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2, 
+                    cv2.putText(img_, f'bus: {bus_south}', (200, 100), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2, 
+                                    color=(0, 0, 255), thickness=4)
+                    
+                    cv2.putText(img_, f'truck: {truck_south}', (200, 150), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2, 
+                                    color=(0, 0, 255), thickness=4)
+                    
+                    cv2.putText(img_, f'car: {car_north}', (1120, 200), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2, 
                                     color=(0, 255, 0), thickness=4)
                     
+                    cv2.putText(img_, f'bus: {bus_north}', (1120, 250), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2, 
+                                    color=(0, 255, 0), thickness=4)
                     
-                        # if CATEGORY_DICT[cls] =='car':
-                        #     car +=1
-                        # cv2.putText(img_, text, (box[0], box[1]), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=1, 
-                        #                 color=get_color(id), thickness=1)
-                        
-                        # cv2.putText(img_, text, (box[0], box[1]), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=1, 
-                        #                 color=get_color(id), thickness=1)
-                        
-                        # cv2.putText(img_, text, (box[0], box[1]), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=1, 
-                        #                 color=get_color(id), thickness=1)
-
-                    # if int(center[0]) < 591 and int(center[1])<
-                    # if cls==0 # 0:car
-                    
-                    # if cls ==1 # 1:bus
-                    
-                    # if cls ==2 # 2:truck
-                    
-                    # if center
-                    
-                    # if (time.time() - start_time) != 0:
-                    #     cv2.putText(img_, "FPS{0}".format('%.1f' % (1/(time.time() - start_time))),
-                    #                 (100,50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255), 3)
-                
+                    cv2.putText(img_, f'truck: {truck_north}', (1120, 300), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2, 
+                                    color=(0, 255, 0), thickness=4)
+              
                 cv2.imshow('',img_)
+                
+                #Show X, Y coordinate on image
+                # cv2.setMouseCallback('', POINTS)
+                
+                # Pause video
                 # cv2.setMouseCallback('', POINTS)
                 
                 start_time = time.time()
@@ -419,47 +393,7 @@ def draw_Counter(img):
     cv2.fillPoly(img, [pts_n1], (0, 0, 254))
     cv2.fillPoly(img, [pts_n2], (0, 254, 0))
     
-    # CounterLine1
-    # cv2.line(img, (314,198+25), (591,249+25), (0, 0, 255), 50)
-    # cv2.line(img, (314,198-25), (591,249-25), (0, 255, 0), 50)
-    
-    # # CounterLine2
-    # cv2.line(img, (728,265+25), (1093,304+25), (0, 0, 254), 50)
-    # cv2.line(img, (728,265-25), (1093,304-25), (0, 254, 0), 50)
     return img
-
-        
-def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32):
-    # Resize and pad image while meeting stride-multiple constraints
-    shape = img.shape[:2]  # current shape [height, width]
-    if isinstance(new_shape, int):
-        new_shape = (new_shape, new_shape)
-    
-    # Scale ratio (new / old)
-    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
-    if not scaleup:  # only scale down, do not scale up (for better test mAP)
-        r = min(r, 1.0)
-
-    # Compute padding
-    ratio = r, r  # width, height ratios
-    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
-    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
-    if auto:  # minimum rectangle
-        dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
-    elif scaleFill:  # stretch
-        dw, dh = 0.0, 0.0
-        new_unpad = (new_shape[1], new_shape[0])
-        ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
-
-    dw /= 2  # divide padding into 2 sides
-    dh /= 2
-
-    if shape[::-1] != new_unpad:  # resize
-        img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
-    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
-    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
-    img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
-    return img, ratio, (dw, dh)
 
 def plot_img(img, frame_id, results, start_time, fps_counter, save_dir=None, show_image=False, ):
     """
@@ -544,8 +478,6 @@ def save_results(folder_name, seq_name, results, data_type='default'):
         for frame_id, target_ids, tlwhs, clses in results:
             if data_type == 'default':
 
-                # f.write(f'{frame_id},{target_id},{tlwh[0]},{tlwh[1]},\
-                #             {tlwh[2]},{tlwh[3]},{cls}\n')
                 for id, tlwh, cls in zip(target_ids, tlwhs, clses):
                     f.write(f'{frame_id},{id},{tlwh[0]:.2f},{tlwh[1]:.2f},{tlwh[2]:.2f},{tlwh[3]:.2f},{int(cls)}\n')
     f.close()
@@ -630,21 +562,12 @@ if __name__ == '__main__':
     parser.add_argument('--min_area', type=float, default=150, help='use to filter small bboxs')
     parser.add_argument('--track_dataset', type=str, default='seg', help='input source mode (video, Webcam, image)')
     parser.add_argument('--save_videos', action='store_true', help='save tracking results (video)')
-    # parser.add_argument('--save_images', action='store_true', help='save tracking results (image)')
     parser.add_argument('--save_images', action='store_true', help='save tracking results (image)')
     
     # detect per several frames
     parser.add_argument('--detect_per_frame', type=int, default=1, help='choose how many frames per detect')    
     parser.add_argument('--track_eval', type=bool, default=True, help='Use TrackEval to evaluate')
    
-    # args=['--dataset','config_files/yolov7_track_custom.yaml',
-    #     '--data_format', 'yolo', 
-    #     '--tracker', 'bytetrack',
-    #     '--model_path', 'weights/yolov7_tiny_best.pt',
-    #     '--img_size', '640',
-    #     '--track_eval', 'False',
-    #     '--track_dataset', 'Webcam'
-    #     ]  
     opts = parser.parse_args()
 
     # NOTE: read path of datasets, sequences and TrackEval configs
